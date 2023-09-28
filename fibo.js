cc3c929e7cfff2d5eb4feb60963ebf34eb344203
Ankit Patidar <95223229+ankit-ml12@users.noreply.github.com>
2023-09-28 11:20:33 +0530

    Update fibo.js

--- fibo.js
+++ fibo.js
@@ -11,3 +11,26 @@ const n = 10;
 for (let i = 0; i < n; i++) {
   console.log(fibonacci(i));
 }
+
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  }
+
+  let fibPrev = 0;
+  let fibCurrent = 1;
+
+  for (let i = 2; i <= n; i++) {
+    const temp = fibCurrent;
+    fibCurrent = fibPrev + fibCurrent;
+    fibPrev = temp;
+  }
+
+  return fibCurrent;
+}
+
+// Test the function with a sample value
+const n = 10;
+for (let i = 0; i < n; i++) {
+  console.log(fibonacci(i));
+}
